ed8c4473195750c3700342539b3c439d7c71c8a9
liufei20151013 <2199216370@qq.com>
2024-06-18 15:19:20 +0800

    refactor: 日志输出优化

--- apps/itsm/sync_to_js.py
+++ apps/itsm/sync_to_js.py
@@ -14,7 +14,7 @@ logger = get_logger(__name__)
 def sync_local_mfa_to_other_js(username, otp_secret_key):
     enabled = settings.ITSM_SYNC_JS_MFA_DATA_ENABLED
     if not enabled:
-        logger.info('当前本地用户 MFA 重置后，同步到其它 JumpServer 环境的功能未开启, 不需要处理')
+        print('当前本地用户 MFA 重置后，同步到其它 JumpServer 环境的功能未开启, 不需要处理')
         return
 
     # 多地址用英文分号分隔
@@ -22,34 +22,34 @@ def sync_local_mfa_to_other_js(username, otp_secret_key):
         jms_url_arr = str(settings.ITSM_SYNC_JS_MFA_HOST).split(';')
         key_id_arr = str(settings.ITSM_SYNC_JS_MFA_AK).split(';')
         secret_id_arr = str(settings.ITSM_SYNC_JS_MFA_SK).split(';')
-        logger.info("JS url size: {}".format(len(jms_url_arr)))
-        logger.info("JS Access Key size: {}".format(len(key_id_arr)))
-        logger.info("JS Secret Key size: {}".format(len(secret_id_arr)))
+        print("JS url size: {}".format(len(jms_url_arr)))
+        print("JS Access Key size: {}".format(len(key_id_arr)))
+        print("JS Secret Key size: {}".format(len(secret_id_arr)))
     except Exception as e:
-        traceback.logger.info_exc()
-        logger.error('账号参数配置不规范：{}'.format(e))
+        traceback.print_exc(e)
+        print('账号参数配置不规范：{}'.format(e))
         return
 
     for index, jms_url in enumerate(jms_url_arr):
         try:
-            logger.info("Handling data, index: {}".format(index + 1))
-            logger.info("Handling data, jms_url: {}".format(jms_url))
+            print("Handling data, index: {}".format(index + 1))
+            print("Handling data, jms_url: {}".format(jms_url))
             KeyID = key_id_arr[index]
             SecretID = secret_id_arr[index]
             auth = get_auth(KeyID, SecretID)
 
-            # todo 根据 username 查询用户
+            # 根据 username 查询用户
             users = get_user(jms_url, auth, username)
             if users and len(users) > 0:
                 user_id = users[0].get('id', '')
             else:
-                logger.error('User[{}] does not exist.'.format(username))
+                print('User[{}] does not exist.'.format(username))
                 continue
 
             update_user_mfa(jms_url, auth, user_id, username, otp_secret_key)
         except Exception as e:
-            traceback.logger.info_exc()
-            logger.error('同步 MFA 的接口调用失败：{}'.format(e))
+            traceback.print_exc(e)
+            print('同步 MFA 的接口调用失败：{}'.format(e))
             continue
 
 
@@ -78,9 +78,9 @@ def update_user_mfa(jms_url, auth, user_id, username, otp_secret_key):
     data = {"username": username, "otp_secret_key": otp_secret_key}
     response = requests.put(url, data=data, headers=headers, auth=auth)
     if response.status_code == 200:
-        logger.info("Success to update user[{}]'s MFA.".format(username, response.reason))
+        print("Success to update user[{}]'s MFA.".format(username, response.reason))
     else:
-        logger.error("Failed to update user[{}]'s MFA, error:{}.".format(username, response.reason))
+        print("Failed to update user[{}]'s MFA, error:{}.".format(username, response.reason))
 
 
 def get_auth(KeyID, SecretID):
