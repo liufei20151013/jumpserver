f650c4a3dcc6e6b0dc7b538ca8562fc39332dd50
liufei20151013 <2199216370@qq.com>
2025-04-25 20:56:35 +0800

    feat: 支持 ak/sk 和账号密码两种方式获取 token

--- apps/authentication/api/token.py
+++ apps/authentication/api/token.py
@@ -12,10 +12,42 @@ from ..mixins import AuthMixin
 
 logger = get_logger(__name__)
 
-__all__ = ['TokenCreateApi']
+__all__ = ['TokenCreateApi', 'ISCTokenCreateApi']
 
 
 class TokenCreateApi(AuthMixin, CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.BearerTokenSerializer
+
+    def create_session_if_need(self):
+        if self.request.session.is_empty():
+            self.request.session.create()
+            self.request.session.set_expiry(600)
+
+    def create(self, request, *args, **kwargs):
+        self.create_session_if_need()
+        # 如果认证没有过，检查账号密码
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            user = self.get_user_or_auth(serializer.validated_data)
+            # self.check_user_mfa_if_need(user)
+            self.check_user_login_confirm_if_need(user)
+            self.send_auth_signal(success=True, user=user)
+            resp = super().create(request, *args, **kwargs)
+            self.clear_auth_mark()
+            return resp
+        except errors.AuthFailedError as e:
+            return Response(e.as_data(), status=400)
+        except errors.NeedMoreInfoError as e:
+            return Response(e.as_data(), status=200)
+        except errors.MFAUnsetError:
+            return Response({'error': 'MFA unset, please set first'}, status=400)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
+
+
+class ISCTokenCreateApi(AuthMixin, CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = serializers.BearerTokenSerializer
 
@@ -29,7 +61,7 @@ class TokenCreateApi(AuthMixin, CreateAPIView):
             self.create_session_if_need()
             user = self.request.user
             logger.info('User[{}] created token!'.format(user.id))
-            self.check_user_mfa_if_need(user)
+            # self.check_user_mfa_if_need(user)
             self.check_user_login_confirm_if_need(user)
             self.send_auth_signal(success=True, user=user)
             resp = super().create(request, *args, **kwargs)
